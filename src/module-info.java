2199fb494ef8fdcc7106706216f1023be00efbc1
MakWil <163393915+MakWil@users.noreply.github.com>
2024-12-11 16:57:08 +0700

    module info commit

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module module {
+	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.base;
+	opens main to javafx.graphics;
+}
