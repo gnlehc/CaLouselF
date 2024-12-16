5b93168c319a30693c1c72dec4ae3b306324effe
hansonivander <hansonivander@gmail.com>
2024-12-16 11:28:33 +0700

    feat: Model & Controller for Transaction

---
+++ src/controllers/TransactionController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class TransactionController {
+	public boolean purchaseItems(String userId, String itemId) {
+		return true;
+	}
+	
+	public void viewHistory(String userId) {
+		
+	}
+	
+	public void createTransaction(String transactionId) {
+		
+	}
+}
---
+++ src/models/Transaction.java
@@ -0,0 +1,49 @@
+package models;
+
+public class Transaction {
+	private String userId;
+	private String itemId;
+	private String transactionId;
+	
+	public Transaction(String userId, String itemId, String transactionId) {
+		this.userId = userId;
+		this.itemId = itemId;
+		this.transactionId = transactionId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+	
+	public boolean purchaseItems(String userId, String itemId) {
+		return true;
+	}
+	
+	public void viewHistory(String userId) {
+		
+	}
+	
+	public void createTransaction(String transactionId) {
+		
+	}
+}
